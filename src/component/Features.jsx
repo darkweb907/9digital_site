3a4a0fed5f3873b0ae730f06ad7197c85b1012de
darkweb907 <barryopeyemi@gmail.com>
2026-06-12 17:33:20 +0100

    remove sanity

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div>Features</div>
+   <section className="feature common-padding scrim-max-width">
+    <div className='w-full mb-12'>
+        <h1 className='section-heading'>Explore the full story.</h1>
+    </div>
+   </section>
   )
 }
 
